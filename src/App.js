cd91d9edc2fc4dad108137db3038b8300d988850
Chipyoka Mwape <mwapechipyoka18@gmail.com>
2024-05-29 21:42:44 +0200

    Start Commit of Web Dev

--- src/App.js
+++ src/App.js
@@ -44,45 +44,5 @@ function App() {
 
 export default App;
 
-// import React, {useState, useEffect} from "react";
-// import {ipcRenderer} from "electron";
 
-// function App() {
-// 	const [note, setNote] = useState([]);
-// 	const [error, setError] = useState(null);
 
-// 	useEffect(() => {
-// 		ipcRenderer.send("fetch-data");
-
-// 		ipcRenderer.on("fetch-data-response", (event, data) => {
-// 			setNote(data);
-// 		});
-
-// 		ipcRenderer.on("fetch-data-error", (event, message) => {
-// 			setError(message);
-// 		});
-
-// 		// Cleanup function to remove event listeners on component unmount
-// 		return () => {
-// 			ipcRenderer.removeAllListeners("fetch-data-response");
-// 			ipcRenderer.removeAllListeners("fetch-data-error");
-// 		};
-// 	}, []);
-
-// 	return (
-// 		<div>
-// 			<h1>Notes List</h1>
-// 			{error && <p style={{color: "red"}}>Error: {error}</p>}
-// 			{note.length === 0 && <p>No clients found.</p>}
-// 			<ul>
-// 				{note.map((note) => (
-// 					<li key={note.NOTE_ID}>
-// 						{note.NOTE_CATEGORY} - {note.NOTE.DATE}
-// 					</li>
-// 				))}
-// 			</ul>
-// 		</div>
-// 	);
-// }
-
-// export default App;
--- src/preload.js
+++
@@ -1,8 +0,0 @@
-// src/main/preload.js
-const {contextBridge, ipcRenderer } = require("electron");
-
-console.log("from preload");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-	fetchData: () => ipcRenderer.invoke("fetch-data")
-});
